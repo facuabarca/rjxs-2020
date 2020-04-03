0040b20b4d7d49a791f2d7db7253920ca8f3cca6
Facundo Abarca <inditex1@P20058.local>
2020-04-03 22:17:50 +0200

    first, take, takeWhile

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,14 +1,20 @@
 import { fromEvent } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { map, first, take, takeWhile } from 'rxjs/operators';
 
 export default () => {
 	/** start coding */
 	const grid = document.getElementById('grid');
 
 	const click$ = fromEvent(grid, 'click').pipe(
-		tap(val => console.log('before::', val)),
+
 		map(val => [Math.floor(val.offsetX / 50), Math.floor(val.offsetY / 50)]),
-		tap(val => console.log('after::', val))
+		// first(val => val[0] > 3)
+
+		// take(4)
+
+		takeWhile(val => val[0] > 3)
+
+
 
 	)
 
