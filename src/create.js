23728473eebbd5a61de680abb7fc3553b70aa81c
Facundo Abarca <inditex1@P20058.local>
2020-04-03 21:50:02 +0200

    fromEvent

--- src/create.js
+++ src/create.js
@@ -1,17 +1,17 @@
 import { displayLog } from './utils';
-import { interval, timer } from 'rxjs';
+import { fromEvent} from 'rxjs';
 
 export default () => {
+	const actionBtn = document.getElementById('action-btn');
+	
+	const source = fromEvent(actionBtn, 'click');
 
-	const source = interval(500);
+	const subscription = source.subscribe(evt => displayLog(`${evt.x}, ${evt.y}`));
 
-	const subscription = source.subscribe(data => displayLog(data));
+	fromEvent(document, 'mousemove').subscribe(evt => {
+		console.log(evt);
+	});
 
-	timer(3000).subscribe(() => subscription.unsubscribe());
+	
 
-	const source2 = timer(4000, 100);
-
-	const subscription2 = source2.subscribe(data => displayLog(`timer::: ${data}`));
-
-	timer(5000).subscribe(() => subscription2.unsubscribe());
 }
